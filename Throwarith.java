32077eae767a6e0a9d58f0eceb2604a6483273f8
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 19:11:03 +0530

    Add files via upload

---
+++ Throwarith.java
@@ -0,0 +1,16 @@
+public class Throwarith{
+    public static void a(int a,int b){
+        if(b==0){
+            throw new ArithmeticException("cannot divide");
+        }
+        System.out.println(a/b);
+    }
+public static void main(String args[]){
+    try{
+        a(10,0);
+    }
+    catch(ArithmeticException e){
+        System.out.println(e.getMessage());
+    }
+}
+}
